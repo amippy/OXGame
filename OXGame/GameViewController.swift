e001a76837743bdab348676a36b6926fa198ce0a
amippy <aminaka1223@gmail.com>
2015-12-13 23:40:42 +0900

    initial

--- OXGame/GameViewController.swift
+++ OXGame/GameViewController.swift
@@ -8,46 +8,36 @@
 
 import UIKit
 import SpriteKit
+  var scene: GameScene!
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,UIGestureRecognizerDelegate
+
+{
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+           }
+       
 
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        let location = sender.locationInView(view)
+        print("tapped at \(location.x), \(location.y)")
+        scene.mark(location)
+        
+    }
+    
 }
